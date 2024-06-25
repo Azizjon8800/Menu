31d76db78508a306526312880dc29bd5113d3066
Azizjon8800 <azizjon8437@gmail.com>
2024-06-25 14:54:12 +0500

    Add project files.

---
+++ Menu/Controllers/MenuController.cs
@@ -0,0 +1,40 @@
+﻿using Menu.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Menu.Controllers
+{
+    public class MenuController : Controller
+    {
+        private readonly AppDbContext _context;
+        public MenuController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index(string searchString)
+        {
+            var dishes  = from d in _context.Dishes select d;
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                dishes = dishes.Where(d => d.Name.Contains(searchString));
+                return View(await dishes.ToListAsync());
+            }
+            return View(await dishes.ToListAsync());
+        }
+
+        public async Task<IActionResult> Detail(int? id)
+        {
+            var dish = await _context.Dishes
+                .Include(di => di.DishIngredients)
+                .ThenInclude(i => i.Ingredient)
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            if (dish == null)
+            {
+                return NotFound();
+            }
+            return View(dish);
+        }
+    }
+}
---
+++ Menu/Migrations/20240620122635_image.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Menu.Migrations
+{
+    /// <inheritdoc />
+    public partial class image : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Dishes",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ImageUrl",
+                value: "https://bravopizza.uz/wp-content/uploads/2020/04/superbravo.jpg");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Dishes",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ImageUrl",
+                value: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fbravopizza.uz%2Fproduct%2Fmargarita%2F&psig=AOvVaw1FOcURrKf_J24OrTKrkVzt&ust=1718971133911000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCKCQrNCQ6oYDFQAAAAAdAAAAABAH");
+        }
+    }
+}
